fa7fbd0a0a377c88fbdfb6ddf7e1ac5adc36f0ba
Lenovo <Lenovo@DESKTOP-NK3DPVS>
2023-09-03 20:35:50 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double uah = 36.84;
+
+            double usd;
+
+            Console.WriteLine("Введите сумму usd");
+
+            usd = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(usd + " usd в uah = " + (usd * uah) + "uah");
+            
+        }
+    }
+}  
+
