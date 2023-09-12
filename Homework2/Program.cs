75e31fe9004038d689c623f5f32ca4967343ab37
lo1d1 <kovalchukyura0405@gmail.com>
2023-09-12 22:25:31 +0300

    виконав дз по малюванню трикутників в консолі

---
+++ Homework2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    internal class Program
+    {
+        static void Main(string[] argsв)
+        {
+            Console.Write("Висота трикутника: ");
+            int height = int .Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++) 
+            {
+                for (int j = 0; j <=i; j++)
+                {                   
+                    Console.Write("!");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.Write("Висота трикутника: ");
+            int heighd2 = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < heighd2; i++)
+            {                
+                for (int j = heighd2; j > i; j--)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
+            Console.Write("Висота трикутника: ");
+            int height3 = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height3; i++)
+            {
+                for (int j = heighd2; j > i; j--)
+                {
+                    Console.Write(" ");
+                }              
+
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("!");
+                }
+                Console.WriteLine();
+            }
+
+            Console.Write("Висота трикутника: ");
+            int heighd4 = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < heighd4; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = heighd4; j > i; j--)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+
+        
+       
+    }
+}
